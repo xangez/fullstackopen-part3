036df0e115d2a8242a3f71d8bd15bf8be2f38bbf
xangez <xangez348@gmail.com>
2020-07-05 00:39:06 +1000

    add build folder

---
+++ build/precache-manifest.9e9eba1b499fa95b9d84b204088be571.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a89a62d56ed8cb407f400cd1b90118ec",
+    "url": "/react-practise/index.html"
+  },
+  {
+    "revision": "0462dec7d84b509a2e4b",
+    "url": "/react-practise/static/css/main.d98c8d4c.chunk.css"
+  },
+  {
+    "revision": "f5178e05c6360e914bac",
+    "url": "/react-practise/static/js/2.ebffb882.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-practise/static/js/2.ebffb882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0462dec7d84b509a2e4b",
+    "url": "/react-practise/static/js/main.8e03607c.chunk.js"
+  },
+  {
+    "revision": "dc3f4ef4e0f444b7113b",
+    "url": "/react-practise/static/js/runtime-main.4be084fa.js"
+  }
+]);
