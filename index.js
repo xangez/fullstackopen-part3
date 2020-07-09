3f53d7b5284a1af7e9e464b03101ee70cbc77af4
xangez <xangez348@gmail.com>
2020-07-09 22:33:51 +1000

    adding numbers works with database

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const morgan = require("morgan");
+const Person = require("./models/person");
+
 const cors = require("cors");
 
 app.use(express.static("build"));
@@ -11,74 +14,56 @@ morgan.token("body", (req, res) => {
   JSON.stringify(req.body);
 });
 
-app.use(
-  morgan(":method :url :status :res[content-length] :body - :response-time ms")
-);
+app.use(morgan(":method :url :status :res[content-length] :body - :response-time ms"));
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123450",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-];
+// app.get("/", (request, response) => {
+//   response.send("<h1>Hello World</h1>");
+// });
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World</h1>");
+// app.get("/info", (request, response) => {
+//   const numberOfPeople = persons.length;
+//   const currentTime = new Date();
+//   const display = `<p>Phonebook has info for ${numberOfPeople} people</p><p>${currentTime}`;
+//   response.send(display);
+// });
+
+app.get("/api/people", (request, response) => {
+  Person.find({}).then((people) => {
+    response.json(people);
+  });
 });
 
-app.get("/info", (request, response) => {
-  const numberOfPeople = persons.length;
-  const currentTime = new Date();
-  const display = `<p>Phonebook has info for ${numberOfPeople} people</p><p>${currentTime}`;
-  response.send(display);
+app.get("/api/people/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+        const display = `<p>${person.name}</p><p>${person.number}</p>`;
+        response.send(display);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
-
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    const display = `<p>${person.name}</p><p>${person.number}</p>`;
-    response.send(display);
-  } else {
-    response.status(404).end();
-  }
-});
-
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  response.status(204).end();
-});
-
-const generateId = () => {
-  return Math.floor(Math.random() * (10000 - 5) + 5);
-};
 
 const checkUnique = (name) => {
-  return persons.find((person) => person.name === name);
+  let unique;
+  Person.find({}).then((people) => {
+    unique = people.find((person) => person.name === name);
+  });
+  return unique;
 };
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/people", (request, response) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
@@ -90,12 +75,15 @@ app.post("/api/persons", (request, response) => {
       error: "name must be unique",
     });
   }
-  const person = {
+
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  };
-  persons = persons.concat(person);
+  });
+
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
   response.send(person);
 });
 
--- mongo.js
+++ mongo.js
@@ -1,35 +1,52 @@
 const mongoose = require("mongoose");
 
-if (process.argv.length > 3) {
+const password = process.argv[2];
+
+const url = `mongodb+srv://blueSky:${password}@cluster0.5yeev.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const name = process.argv[3];
+const number = process.argv[4];
+
+const person = new Person({
+  name: name,
+  number: number,
+});
+
+const printEntries = () => {
+  Person.find({}).then((result) => {
+    console.log("phonebook:");
+    result.forEach((person) => {
+      console.log(person.name, person.number);
+    });
+    mongoose.connection.close();
+  });
+};
+
+const savePerson = () => {
+  person.save().then((result) => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+};
+
+if (process.argv.length < 3) {
   console.log(
     "please provide the password as an argument: node mongo.js <password>"
   );
   process.exit(1);
+} else if (process.argv.length === 3) {
+  printEntries();
+} else {
+  savePerson();
 }
-
-const password = process.argv[2];
-
-const url = `mongodb+srv://blueSky:${password}@cluster0.5yeev.mongodb.net/<note-app>?retryWrites=true&w=majority`;
-
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
-});
-
-const Note = mongoose.model("Note", noteSchema);
-
-const note = new Note({
-  content: "HTML is easy",
-  date: new Date(),
-  important: true,
-});
-
-note.save().then((result) => {
-  console.log("note saved");
-  mongoose.connection.close();
-});
 
 // Fetching objects from the database
